89f170ccd2af1d55900862fcd607bcd9d61c7fa3
ishfaq-ahamed96 <ishfaq.ahamed096@gmail.com>
2022-10-15 16:13:03 +0530

    test commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+var counter = 0;
+var max = 10;
+while(counter++ <= max -1){
+    Console.WriteLine("This counter is:" +counter);
+    if(counter == 10)
+        Console.WriteLine("Counter Ended in :" +counter);
+
+    System.Threading.Thread.Sleep(
+    (int)System.TimeSpan.FromSeconds(1).TotalMilliseconds);
+}
